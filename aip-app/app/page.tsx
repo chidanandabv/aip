1a329a30740de2399399cf595f08faa773273e86
chidanandabv <chidanandabv23@gmail.com>
2024-03-18 22:15:45 -0300

    created components folder and learnd Button cnd UI

--- aip-app/app/page.tsx
+++ aip-app/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    
+<Button variant="destructive">Click Me</Button>    
      )
 }
