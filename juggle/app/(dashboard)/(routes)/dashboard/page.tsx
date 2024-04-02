557582101e6d1e9ba5d85db2cc0042063cd35564
chidanandabv <chidanandabv23@gmail.com>
2024-04-02 06:00:07 +0530

    folder setup completed

---
+++ juggle/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const dashboardpage =()=>{
+    return(
+        <p>
+            Dashboard page (Otherized)
+        </p>
+    )
+}
+
+export default dashboardpage;
---
+++ juggle/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const LandingPage =()=>{
+    return(
+        <div>
+            Landing page (unotherized)
+        </div>
+    )
+}
+
+export default LandingPage;
