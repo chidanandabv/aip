865571a389900932ccd8fa551cf2211439af87f8
chidanandabv <chidanandabv23@gmail.com>
2024-04-14 12:48:51 +0530

    created dashboard and navbar for pc

--- juggle/app/(dashboard)/(routes)/dashboard/page.tsx
+++ juggle/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs"
-
 const dashboardpage =()=>{
     return(
         <div>
         <p>Dashboard page (Otherized)</p>
-        <UserButton afterSignOutUrl="/" />
         </div>
     )
 }
---
+++ juggle/components/mobile-sidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const MobileSidebar = () => {
+    return ( 
+        <Button variant="ghost" size="icon" className="md:hidden"> 
+                <Menu />
+            </Button>
+     );
+}
+ 
+export default MobileSidebar;
