30850e32d08322edb501046b5925773a4350c0d9
chidanandabv <chidanandabv23@gmail.com>
2024-04-17 23:51:19 +0530

    created dashbord completly

--- juggle/app/(dashboard)/(routes)/dashboard/page.tsx
+++ juggle/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { MessageSquare } from "lucide-react";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, Video } from "lucide-react";
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 
  const tools=[{
     label :"conversation",
@@ -10,8 +11,38 @@ import { cn } from "@/lib/utils";
     color:"text-violet-500",
     href:"/conversation"
 
- }]
+ },
+ {
+   label :"Music genration",
+   icon:Music,
+   color:"text-violet-500",
+   href:"/music"
+
+},
+{
+   label :"Image Genration ",
+   icon:ImageIcon,
+   color:"text-violet-500",
+   href:"/image "
+
+},
+{
+   label :"Video Genration",
+   icon:Video,
+   color:"text-violet-500",
+   href:"/video"
+
+},
+{
+   label :"Code Genration",
+   icon:Code,
+   color:"text-violet-500",
+   href:"/code "
+
+} ]
 const dashboardpage =()=>{
+   const router =useRouter ();
+    
     return(
         <div>
              <div className="mb-8 space-y-4 ">
@@ -20,16 +51,22 @@ const dashboardpage =()=>{
              </div>
              <div className="px-4 md:px-20 lg:px-32 space-y-4 ">
                 {tools.map((tool)=>(
-                   <Card key={tool.href}
+                   <Card 
+                   onClick={()=> router.push( tool.href)}
+                   key={tool.href}
                     className="p-4 border-black/5  flex item-center justify-between hover:shadow-md transition cursor-pointer "
                      >
                         <div className="flex items-center gap-x-4 ">
-                            <div className={cn( "p-2 w-fit rounded-md",tool.bgColor)}>
+                            <div className={cn( "p-2 w-fit rounded-md",tool.color)}>
                                 <tool.icon className={cn( " w-8 h-8",tool.color)}/>
+                            </div>
+                            <div className="font-semibold">
+                              {tool.label}
 
                             </div>
 
                         </div>
+                        <ArrowRight className="w-5 h-5"/>
 
                    </Card> 
                 ))}
